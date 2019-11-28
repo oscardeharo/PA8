6e077c706ab41ead9b8a43072c77fecbf2370863
Oscar de Haro <oscardeharo@Oscars-MacBook-Pro.local>
2019-11-28 12:37:56 -0800

    First global variables

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project
+
+Coding a game
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int BOMBNUMBER =10;
+int CELLSIZE = 10;
+int GRIDSIZE = 10;
+
+int main(){
+    int bombArray[BOMBNUMBER];
+    cout<<"HELLO WORLD\n";
+    return 0;
+}
