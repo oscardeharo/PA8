a48b6632d43d039551c574d70227df5f77c7b576
Oscar de Haro <oscardeharovaldez@gmail.com>
2019-12-01 21:28:00 -0800

    Added classes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
-#include<iostream>
-using namespace std;
-int BOMBNUMBER =10;
-int CELLSIZE = 10;
-int GRIDSIZE = 10;
-int WINX = 400;
-int WINY = 400;
-
+#include"Game.h"
 
 int main(){
-    int bombArray[BOMBNUMBER];
-    cout<<"HELLO WORLD\n";
+    Game g;
+    g.print();
     return 0;
 }
+
